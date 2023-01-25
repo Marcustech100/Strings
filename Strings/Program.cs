f0eb7a053a87fce16ecf04d0bb84da4732aa664d
Marcustech100 <mvtechnologic@gmail.com>
2023-01-25 20:34:51 -0300

    Strings estudo

---
+++ Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+ namespace MyApp
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("829629ff-5fce-44e7-8719-0586420f3a37");
+            Console.WriteLine(id);
+        } //Guids
+    }
+ }
